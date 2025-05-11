d8b9788e17974afb22e5d72aff40f70d3432ed24
JettyCoffee <jetty_coffee@outlook.com>
2025-05-11 23:10:55 +0800

    make vercel repoly

--- test.tsx
+++ test.tsx
@@ -7,27 +7,7 @@ const openai = new OpenAI(
     }
 );
 const completion = await openai.chat.completions.create({
-    model: "qwen-omni-turbo",  
-    messages: [{
-        role: "user",
-        content: [
-            {
-                type: "video",
-                video: [
-                    "https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20241108/xzsgiz/football1.jpg",
-                    "https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20241108/tdescd/football2.jpg",
-                    "https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20241108/zefdja/football3.jpg",
-                    "https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20241108/aedbqh/football4.jpg"
-                ]
-            },
-            {
-                type: "text",
-                text: "请根据视频和声音内容，分析画面中人物的情感和心理状态，并给出相应的依据，以json格式返回"
-            }
-        ]
-    }],
-    stream: true,
-    stream_options: {
+
         include_usage: true
     },
     modalities: ["text"],
